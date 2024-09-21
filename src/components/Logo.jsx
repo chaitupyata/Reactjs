5a00d402ff1e1c5e9a617492535c965fe902aaa6
chaitupyata <chaitupyata@gmail.com>
2024-09-21 14:54:16 +0530

    MegaBlog

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://w7.pngwing.com/pngs/763/464/png-transparent-twitter-logo-icon-logo-twitter-logo-marine-mammal-responsive-web-design-graduate-university-thumbnail.png" alt="ironman"  width={60} style={{borderRadius: 100}}/>
+    </div>
+  )
+}
+
+export default Logo
